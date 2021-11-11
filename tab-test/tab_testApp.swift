d5695ca1d1a62bcb475854ae53ee16a60115ba25
Navid Farshchi <navid.farshchi@luxuryescapes.com>
2021-11-11 13:42:48 +1100

    Initial Commit

---
+++ tab-test/tab_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  tab_testApp.swift
+//  tab-test
+//
+//  Created by Navid on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct tab_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
